60706c5367893e99c798f47ba4f08652fe7d7e43
준영 <67959622+jun-yeong1@users.noreply.github.com>
2021-11-01 11:50:57 +0000

    FILE

---
+++ solved/1065.py
@@ -0,0 +1,17 @@
+def Xnum(n):
+  if n >= 1 and n < 100:
+    return True 
+  elif n >= 100 and n <= 1000:
+    onenum = n // 100
+    twonum = (n - (onenum * 100)) // 10
+    threenum = n - (onenum * 100) - (twonum * 10)
+    if twonum - onenum == threenum - twonum:
+      return True
+
+N = int(input())
+count = 0
+for i in range(1, N+1):
+  if Xnum(i) == True:
+    count += 1
+print(count)
+
