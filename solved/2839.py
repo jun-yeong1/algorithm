76e1ff01eedda3e165098dda9a12872d22b03534
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-10-28 22:07:53 +0900

    다이나믹 2839

--- solved/2839.py
+++ solved/2839.py
@@ -1,6 +1,16 @@
-n = int(input())
-array = list(map(int, input().split()))
-array.sort()
-for i in range(5):
-  print(array[i])
-print(type(array[1]))
+n = int(input()) #설탕 키로수
+count = 0 #최소 봉지 갯수
+array = [3, 5]  
+#최대 5000이니깐 5001
+d = [5001] * (n + 1) 
+
+#다이나믹 프로그래밍
+d[0] = 0
+for i in range(2):
+  for j in range(array[i], n + 1):
+    if d[j - array[i]] != 5001:
+      d[j] = min(d[j], d[j -array[i]] + 1)
+if d[n] == 5001:
+  print(-1)
+else:
+  print(d[n])
