6e9e4cedeee41c83487e83f9a0feac1e92330847
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-10-29 23:44:03 +0900

    1469 다이나믹

--- solved/2839.py
+++ solved/2839.py
@@ -8,7 +8,7 @@ d[0] = 0
 for i in range(2):
   for j in range(array[i], n + 1):
     if d[j - array[i]] != 5001:
-      d[j] = min(d[j], d[j -array[i]] + 1)
+      d[j] = min(d[j], d[j - array[i]] + 1)
 if d[n] == 5001:
   print(-1)
 else:
