76c1825119bddfda4106879d1bff40135759f3ed
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-11-21 01:18:35 +0900

    10844 해냄
    리스트 얕은 복사와 깊은 복사 숙지
    import copy 생각

--- solved/10844.py
+++ solved/10844.py
@@ -1,9 +1,29 @@
+import copy
+
 n = int(input())
 
-dp = [[0] * 2 for _ in range(10)]
+dp = [0] * 10
+dpnum = [0] * 10
 
-for i in range(1, n):
-  if i > 0 and i < 9:
-    dp[i][0] = i - 1
-    dp[i][1] = i + 1
+for s in range(1, 10):
+  dp[s] = 1
+  dpnum[s] = 1
   
+for k in range(n - 1):
+  for i in range(0, 10):
+    if i == 0:
+      dp[i] = dpnum[i + 1]
+    elif i == 9:
+      dp[i] = dpnum[i - 1] 
+    else:
+      dp[i] = dpnum[i - 1] + dpnum[i + 1]
+  dpnum = copy.deepcopy(dp)
+  
+result = 0
+if n == 1:
+  print(9)
+else:
+  for j in range(1, 9):
+    result += dp[j]
+  result += dp[0] + dp[9]
+  print(result % 1000000000)
