9a1fc0b2e62acc844a8d17e0fb88fd9bf705d57f
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-11-23 00:24:07 +0900

    2156 해결
    dp를 항상 생각
    dp스럽게 해결하기

--- solved/2156.py
+++ solved/2156.py
@@ -1,17 +1,25 @@
+import sys
+
 n = int(input())
 
 grape = []
 
 for _ in range(n):
-  grape.append(int(input()))
+  grape.append(int(sys.stdin.readline()))
 
 dp = [0] * n
+if n == 1:
+  print(grape[0])
+elif n == 2:
+  print(grape[0] + grape[1])
+elif n == 3:
+  print(sum(grape[:3]) - min(grape[:3]))
+else:
+  dp[0] = grape[0]
+  dp[1] = grape[0] + grape[1]
+  dp[2] = dp[1] + grape[2] - min(grape[:3])
 
-dp[0] = grape[0]
-dp[1] = grape[0] + grape[1]
+  for i in range(3, n):
+    dp[i] = max(dp[i - 1], dp[i - 2] + grape[i], dp[i - 3] + grape[i - 1] + grape[i])
 
-for i in range(n):
-  
-
-
-print(max(dp))
+  print(dp[n - 1])
