282f5ccfa9f09d14bd46c90aeee6b88796687cb2
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-12-29 20:54:38 +0900

    1300
    이분탐색
    3*3은 1의 배수 3개, 2의 배수 3개, 3의 배수 3개

--- solved/1300.py
+++ solved/1300.py
@@ -1,13 +1,18 @@
 n = int(input())
 k = int(input())
 
-array = []
-
-for i in range(n):
-  for j in range(n):
-    array.append((i + 1) * (j + 1))
-    
-array.sort()
-
-print(array[k])
-##내일 다시 풀자
+start = 1
+end = k
+result = 0
+while start <= end:
+  mid = (start + end) // 2
+  tmp = 0
+  # 각 숫자의 배수가 배열
+  for i in range(1, n + 1): 
+    tmp += min(mid // i, n)
+  if tmp < k:
+    start = mid + 1
+  else:
+    result = mid
+    end = mid - 1
+print(result)
