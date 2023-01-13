ff793e25a21deea16fef43f07f3cc266b328d6b2
jun-yeong1 <dlwnsdud1320@gmail.com>
2023-01-13 14:00:51 +0900

    7576 part 2 완
    bfs 좌표 기초

--- solved/7576.py
+++ solved/7576.py
@@ -1,18 +1,12 @@
 import sys
 from collections import deque
-def bfs(x, y):
-  
-  queue.append((x, y))
+def bfs(): # 미리 토마토 위치 저장 후 bfs
   while queue:
     x, y = queue.popleft()
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if nx < 0 or nx >= n or ny < 0 or ny >= m:
-        continue
-      if graph[nx][ny] == 1 or graph[nx][ny] == -1:
-        continue
-      if graph[nx][ny] == 0:
+      if nx >= 0 and nx < n and ny >= 0 and ny < m and graph[nx][ny] == 0:
         graph[nx][ny] = graph[x][y] + 1
         queue.append((nx, ny))
   return graph
@@ -24,11 +18,16 @@ for i in range(n):
 
 dx = [1, -1 , 0, 0]
 dy = [0, 0, -1, 1]
+result = 0
 for i in range(n):
   for j in range(m):
     if graph[i][j] == 1:
-      bfs(i, j)
-if all(0 not in l for l in graph):
-  print(max(map(max, graph))-1)
-else:
-  print(-1)
+      queue.append((i, j))
+bfs()
+for i in graph:
+  for j in i:
+    if j == 0:
+      print(-1)
+      exit(0)
+  result = max(result, max(i))
+print(result - 1)
