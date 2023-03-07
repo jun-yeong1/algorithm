b7821f4b312fbff020ca514d3ec86862eb6b47e2
jun-yeong1 <dlwnsdud1320@gmail.com>
2023-03-07 23:51:24 +0900

    과제1 완

--- university/main.cpp
+++ university/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    int num1, num2, i = 0;
+    int num1 = 0, num2 = 0, i = 0;
     for (i = 1; i <= 100; i++) {
         num1 += i;
-        if (i / 3 == 0) {
+        if (i % 3 == 0) {
             num2 += i;
         }
     }
-    cout << "1~100 사이의 정수합은" << num1 << "이고" << endl;
-    cout << "3의 배수의 합은" << num2 << "입니다";
+    cout << "1~100 사이의 정수합은 " << num1 << "이고" << endl;
+    cout << "3의 배수의 합은 " << num2 << "입니다";
 }
