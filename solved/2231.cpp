fbf44efea930e57291cf0a11512fd6c5431b0581
jun-yeong1 <dlwnsdud1320@gmail.com>
2023-08-26 21:19:08 +0900

    2231 덜 품
    덜 품

---
+++ solved/2231.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  // 입력받은 수 자리수 구하기
+  int len = n;
+  int count = 0;
+  while (len != 0) {
+    len /= 10;
+    count++;
+  }
+  //가장 작은 생성자
+  int result = 1-;
+  for (int i = (count * 9); i < 0; i--) {
+    int min_n = n - i;
+  }
+}
