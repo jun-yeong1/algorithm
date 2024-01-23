0bb4411928493c6dea40a4a80d86279efeefcf82
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-01-23 17:50:46 +0900

    2629 냅색 문제 해결

--- solved/2629.cpp
+++ solved/2629.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 // ch = ?? ??
 int ch;
-int ch_array[30];
-int cache[40001][40001];
+int ch_array[31];
+bool cache[40500][31];
 
-int check(int left, int right, int chu) {
-    if (left == right) return 1;
-    if (chu == ch && left != right) return 0;
-    int& ret = cache[left][right];
-    if (ret != -1) return ret;
-    ret = chu;
-    check(left, right, chu+1);
-    check(left+ch_array[chu], right, chu+1);
-    check(left, right+ch_array[chu], chu+1);
+void check(int weight, int chu) {
+    if (chu > ch || cache[weight][chu]) return;
+    cache[weight][chu] = true;
+    // ? ??
+    check(weight, chu+1);
+    // ??? ??
+    check(abs(weight - ch_array[chu]), chu+1);
+    // ?? ??
+    check(weight + ch_array[chu], chu+1);
 }
 
 int main(void) {
@@ -22,12 +22,13 @@ int main(void) {
     for (int i = 0; i < ch; i++) {
         cin >> ch_array[i];
     }
-    fill(&cache[0][0], &cache[40000][40001], -1);
+    check(0, 0);
     int cicle;
     cin >> cicle;
     for (int i = 0; i < cicle; i++) {
         int check_ci;
         cin >> check_ci;
-        cout << check(check_ci, 0, 0) << " ";
+        if (cache[check_ci][ch]) cout << "Y" << " ";
+        else cout << "N" << " "; 
     }
 }
