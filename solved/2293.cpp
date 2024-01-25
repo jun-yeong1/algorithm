c5cf8ac09bc0f8381db37846618e5f4d33c9a66a
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-01-25 16:48:02 +0900

    2293 반복 동적계획법

---
+++ solved/2293.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int n, k;
+int coin[100], sum[10001];
+
+int coin_n() {
+    sum[0] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = coin[i]; j <= k; j++) {
+            sum[j] = sum[j] + sum[j - coin[i]];
+        }
+    }
+    return sum[k];
+}
+
+
+int main(void) {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> coin[i];
+    }
+    cout << coin_n();
+}
