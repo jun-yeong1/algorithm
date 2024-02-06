26849c3561f343a3dcf884608a3b746b89996141
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-02-06 16:02:17 +0900

    17404 동적계획법

--- solved/17404.cpp
+++ solved/17404.cpp
@@ -3,24 +3,27 @@
 using namespace std;
 
 int n;
-int cost[1000][3], cache[1000][1<<3];
-
+int cost[1000][3], cache[1000][3];
+int result;
 // 현재 집, 그전 집 색
-int color(int start, int colored) {
-    int& ret = cache[start][colored];
-    if (ret != -1) return ret;
-    ret = 1e9;
-    for (int i = 0; i < n; i++) {
-        if ()
-        if (colored & (1<<1)) {
-            ret = min(ret, color(i, colored | (1<<2)) + cost[i][1]);
-            ret = min(ret, color(i, colored | (1<<3)) + cost[i][2]);
-        } else if (colored & (1<<2)) {
-            ret = min(ret, color(i, colored | (1<<1)) + cost[i][0]);
-            ret = min(ret, color(i, colored | (1<<3)) + cost[i][2]);
-        } else if (colored & (1<<3)) {
-            ret = min(ret, color(i, colored | (1<<1)) + cost[i][0]);
-            ret = min(ret, color(i, colored | (1<<2)) + cost[i][1]);
+void color() {
+    result = 1e9;
+    for (int RGB = 0; RGB < 3; RGB++) {
+        for (int i = 0; i < 3; i++) {
+            if (i == RGB) {
+                cache[0][i] = cost[0][i];
+            } else {
+                cache[0][i] = 1e9;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            cache[i][0] = cost[i][0] + min(cache[i-1][1], cache[i-1][2]);
+            cache[i][1] = cost[i][1] + min(cache[i-1][0], cache[i-1][2]);
+            cache[i][2] = cost[i][2] + min(cache[i-1][0], cache[i-1][1]);
+        }
+        for (int i = 0; i < 3; i++) {
+            if (i == RGB) continue;
+            else result = min(result, cache[n-1][i]);
         }
     }
 }
@@ -30,5 +33,7 @@ int main(void) {
     for (int i = 0; i < n; i++) {
         cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
     }
-    memset(cache, -1, sizeof(cache));
+    memset(cache, 0, sizeof(cache));
+    color();
+    cout << result;
 }
