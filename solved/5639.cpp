336fec2ee075404b4524370484feb8f7b0f3c2ce
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-02-10 00:44:07 +0900

    5639 아직 못품

---
+++ solved/5639.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> preOrder;
+
+void postOrder(int pStart, int pEnd) {
+    if (pStart >= pEnd) return;
+    // 크기
+    int N = pEnd - pStart;
+    // 해당 트리 root 노드
+    int root = preOrder[pStart];
+    int L_i;
+    for (int i = pStart; i < pEnd; i++) {
+        if (preOrder[i] > root) {
+            L_i = i;
+            break;
+        } else L_i = pEnd-1;
+    }
+    // left node size
+    int L = L_i - pStart;
+    postOrder(pStart+1, pStart+L);
+    postOrder(pStart+L, pStart+N-1);
+    // 후위 순회 출력
+    cout << root << '\n';
+}
+
+int main(void) {
+    int a;
+    // 입력을 받았으면 true
+    while (cin >> a) {
+        preOrder.push_back(a);
+    }
+    postOrder(0, preOrder.size());
+}
