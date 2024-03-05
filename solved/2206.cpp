2122ddf7bee0d023c5c3648911f3daa4d098b054
jun-yeong1 <dlwnsdud1320@gmail.com>
2024-03-05 21:36:52 +0900

    2206 BFS 응용문제
    붙어있는 숫자 입력받을 시
    c언어나 string 이용

--- solved/2206.cpp
+++ solved/2206.cpp
@@ -19,33 +19,38 @@ int visited[1000][1000][2];
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-void bfs(int x, int y) {
+int bfs(int x, int y) {
     queue<qu> q;
-    q.push({0, 0, 0});
     visited[0][0][0] = 1;
+    q.push({0, 0, 0});
     while (!q.empty()) {
         int x = q.front().x;
         int y = q.front().y;
         int wall = q.front().wall;
         q.pop();
+        if (x == n-1 && y == m-1) return visited[x][y][wall];
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
+            int nwall = wall;
             // 공간을 넘어갔을 때
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            // 방문했던 장소 넘어감
+            if (visited[nx][ny][wall]) continue;
             // 벽 있음, 벽 부순적 없음, 방문 안함
-            if (graph[nx][ny] == 1 && wall == 0 && !visited[nx][ny][wall+1]) {
-                visited[nx][ny][wall + 1] = true;
-                result[nx][ny] = result[x][y] + 1;
-                q.push({nx, ny, wall+1});
+            if (graph[nx][ny] == 1 && nwall == 0) {
+                visited[nx][ny][1] = visited[x][y][wall] + 1;
+                nwall = 1;
+                q.push({nx, ny, nwall});
             }
             // 벽 없음, 방문 안함
-            if (graph[nx][ny] == 0 && !visited[nx][ny][wall]) {
-                result[nx][ny] = result[x][y] + 1;
+            if (graph[nx][ny] == 0) {
+                visited[nx][ny][nwall] = visited[x][y][wall] + 1;
                 q.push({nx, ny, wall});
             }
         }
     }
+    return -1;
 }
 int main(void) {
     cin >> n >> m;
@@ -54,11 +59,6 @@ int main(void) {
             scanf("%1d", &graph[i][j]);
         }
     }
-    bfs(0, 0);
-    if (result[n - 1][m - 1] == 0) {
-        cout << -1;
-    } else {
-        cout << result[n - 1][m - 1] + 1;
-    }
+    cout << bfs(0, 0);
     return 0;
 }
