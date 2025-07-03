c61950f52c9d5e1dd94229f38641a82b60de51a8
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-03 21:18:32 +0900

    (백트래킹) 15652

---
+++ c++/15652.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void back(bool visited[], int arr[], int n, int m, int num) {
+    if (num == m) {
+        for (int i = 0; i < m-1; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("%d\n", arr[m-1]);
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (num >= 1 && arr[num-1] > i+1) continue;
+        if (num == m-1 && visited[i] == false) {
+            visited[i] = true;
+            arr[num] = i+1;
+            num++;
+            back(visited, arr, n, m, num);
+            num--;
+            visited[i] = false;
+        } else {
+            arr[num] = i+1;
+            num++;
+            back(visited, arr, n, m, num);
+            num--;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    bool visited[n];
+    int arr[m];
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+    back(visited, arr, n, m, 0);
+
+    return 0;
+}
