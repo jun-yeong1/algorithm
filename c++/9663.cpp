7fab11e72b1968b1a17e63e34f1d50cdaba6a175
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-04 23:24:52 +0900

    (백트래킹) 9663
    N-queen

---
+++ c++/9663.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 15
+using namespace std;
+
+int board[MAX]; // board[행] = 값(열) 반대도 됨
+int n;
+int cnt;
+
+void path(int y) {
+	int ko;
+	if(y == n) {
+		cnt++;
+		return;
+	}
+	for(int i = 0; i < n; i++) {
+		ko = 1;
+		for(int j = 0; j < y; j++) {
+            // 같은 행이나 열에 있는지, 대각선에 있는지
+			if(board[j] == i || abs(y-j) == abs(i-board[j])) {
+				ko = 0;
+				break;
+			}
+		}
+		if(ko) {
+			board[y] = i;
+			path(y+1);
+		}
+	}
+}
+
+int main() {
+    cin >> n;
+    cnt = 0;
+    path(0);
+
+    cout << cnt << '\n';
+
+	return 0;
+}
