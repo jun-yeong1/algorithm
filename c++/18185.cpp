9690e2a8fe4227bab916534e399c3c9384d6925a
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-06 23:52:01 +0900

    (그리디) 18185 해결
    참고해서 풀어서 더 고민이 필요함

--- c++/18185.cpp
+++ c++/18185.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
-
-int min_num(int a, int b, int c) {
-    if (a >= b) {
-        if (b >= c) {
-            return c;
-        } else {
-            return b;
-        }
-    } else {
-        if (a >= c) {
-            return c;
-        } else {
-            return a;
-        }
-    }
-}
 
 int main () {
     int result = 0;
@@ -26,59 +11,57 @@ int main () {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    cout << endl;
-    for (int i = 0; i < n-2; i++) {
-        if (arr[i] == 0 || arr[i+1] == 0 || arr[i+2] == 0) continue;
-        int a = min_num(arr[i], arr[i+1], arr[i+2]);
-        result += a * 7;
-        arr[i] -= a; arr[i+1] -= a; arr[i+2] -= a;
-    }
-    for (int i = 0; i < n-1; i++) {
-        if (arr[i] == 0 || arr[i+1] == 0) continue;
-        int b = 0;
-        if (arr[i] > arr[i+1]) {
-            b = arr[i+1];
-            arr[i] -= b; arr[i+1] -= b;
-            result += b * 5;
-        } else {
-            b = arr[i];
-            arr[i] -= b; arr[i+1] -= b;
-            result += b * 5;
-        }
-    }
+
     for (int i = 0; i < n; i++) {
         if (arr[i] == 0) continue;
-        result += arr[i] * 3;
+        int num = arr[i];
+        arr[i] = 0;
+        result += num * 3;
+        
+        if (i+1 >= n) continue;
+        num = min(num, arr[i+1]);
+        arr[i+1] -= num;
+        result += num * 2;
+
+        if (i+2 >= n) continue;
+        if (num + arr[i+1] > arr[i+2]) { // 원래 arr[i+1] 크기와 비교
+            // 앞에서 뺀 후의 arr[i+1]가 arr[i+2] 보다 커야
+            // 3번째도 arr[i]의 수를 빼주어야 함
+            // 만약 같거나 작은 경우는 arr[i+2]는 빼지 않음
+            num = min(num, max(0, arr[i+2]-arr[i+1]));
+        }
+        arr[i+2] -= num;
+        result += num * 2;
     }
     cout << result;
     return 0;
 }
-// 2부터 먼저 지워야 하는 케이스
-4
-1 2 1 1
-// 정답 : 12
+// // 2부터 먼저 지워야 하는 케이스
+// 4
+// 1 2 1 1
+// // 정답 : 12
     
-// 2부터 먼저 지워야 하는 케이스
-4
-2 4 2 2 
-// 정답 : 24
+// // 2부터 먼저 지워야 하는 케이스
+// 4
+// 2 4 2 2 
+// // 정답 : 24
     
-// 2부터 먼저 지워야 하는 케이스
-4
-1 4 3 3 
-// 정답 : 26
+// // 2부터 먼저 지워야 하는 케이스
+// 4
+// 1 4 3 3 
+// // 정답 : 26
     
-// 3부터 먼저 지워야 하는 케이스
-4
-1 3 3 1
-// 정답 : 19
+// // 3부터 먼저 지워야 하는 케이스
+// 4
+// 1 3 3 1
+// // 정답 : 19
     
-// 2, 3을 연속해서 지워야 하는 케이스
-4
-2 5 4 2
-// 정답 : 31
+// // 2, 3을 연속해서 지워야 하는 케이스
+// 4
+// 2 5 4 2
+// // 정답 : 31
     
-// 여러 가지 숫자 케이스
-7
-2 3 2 3 3 2 1 
-// 정답 : 38
+// // 여러 가지 숫자 케이스
+// 7
+// 2 3 2 3 3 2 1 
+// // 정답 : 38
