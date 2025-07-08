95a55414fd5ebc5b4511435fa49ff8adeb2a568c
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-08 20:53:07 +0900

    (그리디)2720 완

--- c++/2720.cpp
+++ c++/2720.cpp
@@ -12,19 +12,19 @@ int main() {
     for (int i = 0; i < T; i++) {
         int coin[4] = {0, 0, 0, 0};
         if (arr[i] >= 25) {
-            coin[0] = coin[0] / 25;
+            coin[0] = arr[i] / 25;
             arr[i] = arr[i] % 25;
         }
         if (arr[i] >= 10) {
-            coin[1] /= 10;
+            coin[1] = arr[i] / 10;
             arr[i] %= 10;
         }
         if (arr[i] >= 5) {
-            coin[2] /= 5;
+            coin[2] = arr[i] / 5;
             arr[i] %= 5;
         }
         if (arr[i] >= 1) {
-            coin[3] /= 1;
+            coin[3] = arr[i] / 1;
             arr[i] %= 1;
         }
         cout << coin[0] << " " << coin[1] << " " << coin[2] << " " << coin[3] << endl;
