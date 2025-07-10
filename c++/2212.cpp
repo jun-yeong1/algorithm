156d7bcdb5b6056c238422eb3d4204943daa0bc6
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-10 19:58:03 +0900

    (그리디) 2212 미완

---
+++ c++/2212.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N;
+    cin >> K;
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    return 0;
+}
