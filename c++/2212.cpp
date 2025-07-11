8ebe863c1f54dd3db4abbfd6c54b5c7d9d80081b
jun-yeong1 <dlwnsdud1320@gmail.com>
2025-07-11 23:24:28 +0900

    (그리디) 2212 미완2

--- c++/2212.cpp
+++ c++/2212.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
-
+#define MAX 10000
 using namespace std;
+int N, K;
+int arr[10000];
 
+int min(int start) {
+    int min = 1000001;
+    int num = 0;
+    for (start; start < N; start++) {
+        if (arr[start] < min) {
+            min = arr[start];
+            num = start;
+        }
+    }
+    return num;
+}
+void change(int a, int b) {
+    int tmp = 0;
+    tmp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = tmp;
+}
+
+void sort() {
+    for (int i = 0; i < N; i++) {
+        change(min(i), i);
+    }
+}
 int main() {
-    int N, K;
     cin >> N;
     cin >> K;
-    int arr[N];
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
+    sort();
+    for (int i = 0; i < N; i++) {
+        cout << arr[i];
+    }
+
     return 0;
 }
